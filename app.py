caeb1e48de387de9982d3e7b3b2a82dce685605c
kushal-10 <kushalkoshti11@gmail.com>
2024-11-11 15:25:32 +0100

    update bg

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import gradio as gr
-import numpy as np
-import plotly.graph_objects as go
 
 from src.utils import select_board
 
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ def select_board(level: str = 'easy', size: int = 18, board_number: int = 0):
     initial_board_image, target_positions, info = GenerateBoard(level, size, board_number).setup_initial_board() 
 
     # Convert initial_board_image to a Plotly figure
-    fig = px.imshow(initial_board_image)  # Use Plotly's imshow
+    fig = px.imshow(initial_board_image)  # Use Plotly's imshow with gray scale
     fig.update_xaxes(showticklabels=False)  # Hide x-axis ticks
     fig.update_yaxes(showticklabels=False)  # Hide y-axis ticks
 
